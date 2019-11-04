327a074a11cfb83bc401e9515f7d8988f1ffa3a9
Benjamin L. Morrison <newben.hd@gmail.com>
2019-11-04 14:53:30 -0800

    added hot reloading to webpack and served client side code for production mode

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -10,10 +11,26 @@ app.use(express.urlencoded({
 }));
 app.use(cors());
 
+app.use(express.static(path.resolve(__dirname, '../public')));
+
 app.get('/api', (req, res) => {
   res.status(200).json({
     message: 'Hello, World',
   });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+  console.log(path.resolve(__dirname, '../public/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const errorTemplate = {
+    status: 500,
+    msg: 'Please try again.',
+  };
+  const error = Object.assign(errorTemplate, err);
+  res.status(error.status).send(error.msg);
 });
 
 app.listen(PORT, () => {
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './client/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   mode: process.env.NODE_ENV || 'production',
